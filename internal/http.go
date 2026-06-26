0e6591e575621a7aaf0e4cb0b7c206b5998f430d
radin-111 <radinsheikh87@gmail.com>
2026-06-26 19:29:11 +0600

    fixing some issues and adding required built-in middlewares

--- internal/http.go
+++ internal/http.go
@@ -5,20 +5,39 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 	"gorm.io/gorm"
 )
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
 func Start(db *gorm.DB, cfg *config.Config) {
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+	e.Use(middleware.CORS())
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestLogger())
 
-	e.GET("/", func(c echo.Context) error {
+	e.GET("/", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
 			"message": "Hello, World!",
 		})
 	})
 
 	port := fmt.Sprintf(":%s", cfg.Port)
+
 	if err := e.Start(port); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
